3f54460c6de1357b04035c62369f21180f1ef70a
Kate <kate.gornostaeva@inbox.ru>
2025-03-24 16:35:11 +0300

    немножко создание тестов

--- src/main/java/ru/kate/ebook/nodes/EditableTestSection.java
+++ src/main/java/ru/kate/ebook/nodes/EditableTestSection.java
@@ -1,8 +1,6 @@
 package ru.kate.ebook.nodes;
 
-import javafx.scene.control.Button;
-import javafx.scene.control.ContentDisplay;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
@@ -14,10 +12,12 @@ public class EditableTestSection extends VBox {
 
     private TextField txtQuestion;
     private VBox vBoxAnswers;
+    private boolean oneIs = true;
+    ToggleGroup group;
 
     public EditableTestSection() {
         super();
-        setStyle("-fx-background-color: #888; -fx-padding: 15");
+        setStyle("-fx-background-color: #888; -fx-padding: 15; -fx-spacing: 15;");
 
         txtQuestion = new TextField();
         txtQuestion.setPromptText("Введите вопрос");
@@ -28,6 +28,13 @@ public class EditableTestSection extends VBox {
         imageView.setFitHeight(16);
         editButton.setGraphic(imageView);
         editButton.setContentDisplay(ContentDisplay.LEFT);
+        editButton.setOnAction(e -> {
+            oneIs = !oneIs;
+            //по хорошему надо сохранить введённые варианты
+            getChildren().remove(vBoxAnswers);
+            //а тут их восстановить
+            getChildren().add(getVBoxAnswers());
+        });
 
         HBox hBox = new HBox();
         hBox.setSpacing(10);
@@ -41,9 +48,63 @@ public class EditableTestSection extends VBox {
         getChildren().add(hBox);
         VBox.setVgrow(hBox, Priority.ALWAYS);
 
+        getChildren().add(getVBoxAnswers());
+    }
 
+    private VBox getVBoxAnswers() {
         vBoxAnswers = new VBox();
-        getChildren().add(vBoxAnswers);
+        vBoxAnswers.setStyle("-fx-background-color: #aaa; -fx-padding: 15; -fx-spacing: 15;");
+        if (oneIs) {
+            group = new ToggleGroup();
+            RadioButton radioButton = new RadioButton();
+            radioButton.setToggleGroup(group);
+            TextField textField = new TextField();
+            textField.setPromptText("Вариант ответа");
+            Button newButton = new Button("Добавить вариант");
+            HBox hBox = new HBox();
+            hBox.setSpacing(10);
+            hBox.getChildren().add(radioButton);
+            hBox.getChildren().add(textField);
+            vBoxAnswers.getChildren().add(hBox);
+            vBoxAnswers.getChildren().add(newButton);
 
+            newButton.setOnAction(e -> {
+                RadioButton radioButton1 = new RadioButton();
+                radioButton1.setToggleGroup(group);
+                TextField textField1 = new TextField();
+                textField1.setPromptText("Вариант ответа");
+                HBox hBox1 = new HBox();
+                hBox1.setSpacing(10);
+                hBox1.getChildren().add(radioButton1);
+                hBox1.getChildren().add(textField1);
+                vBoxAnswers.getChildren().remove(newButton);
+                vBoxAnswers.getChildren().add(hBox1);
+                vBoxAnswers.getChildren().add(newButton);
+            });
+        } else {
+            CheckBox checkBox = new CheckBox();
+            TextField textField = new TextField();
+            textField.setPromptText("Вариант ответа");
+            Button newButton = new Button("Добавить вариант");
+            HBox hBox = new HBox();
+            hBox.setSpacing(10);
+            hBox.getChildren().add(checkBox);
+            hBox.getChildren().add(textField);
+            vBoxAnswers.getChildren().add(hBox);
+            vBoxAnswers.getChildren().add(newButton);
+            newButton.setOnAction(e -> {
+                CheckBox checkBox1 = new CheckBox();
+                TextField textField1 = new TextField();
+                textField1.setPromptText("Вариант ответа");
+                HBox hBox1 = new HBox();
+                hBox1.setSpacing(10);
+                hBox1.getChildren().add(checkBox1);
+                hBox1.getChildren().add(textField1);
+                vBoxAnswers.getChildren().remove(newButton);
+                vBoxAnswers.getChildren().add(hBox1);
+                vBoxAnswers.getChildren().add(newButton);
+            });
+        }
+        return vBoxAnswers;
     }
 }
